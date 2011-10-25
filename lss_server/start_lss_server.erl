21e4098f4a3a585230ca4cda6163692b822bde6e
Alex Shneyderman <a.shneyderman@gmail.com>
2011-10-25 21:35:25 +0200

    Cleanup of not needed files in the previous commit

--- lss_server/start_lss_server.erl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env escript
-%%! -pz /Users/sandor/projects/erlang/lss_n_tools/lss_server/ebin /Users/sandor/projects/erlang/lss_n_tools/lss_parse_utils/ebin 
-
-main(Args) ->
-    application:start(lss_app).
